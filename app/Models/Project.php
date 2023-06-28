7066f6b7f9998caea4bd31671e0a2e9549534fdf
mirkettinho <mirkodifranco30@gmail.com>
2023-06-28 15:32:30 +0200

    [add] relation many to many

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -28,6 +28,14 @@ class Project extends Model
       return $this->belongsTo(Type::class);
     }
 
+
+    public function technologies(){
+      return $this->belongsToMany(Technology::class);
+    }
+
+
+
+
     public static function generateSlug($str){
       $slug = Str::slug($str, '-');
       $original_slug = $slug;
