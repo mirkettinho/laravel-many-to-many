b613edb8de877d7ceb6e35fb1bda10594faaf9c2
mirkettinho <mirkodifranco30@gmail.com>
2023-06-28 16:06:55 +0200

    [add] pivot table seeder

---
+++ database/seeders/ProjectsTechnologiesTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Project;
+use App\Models\Technology;
+
+
+
+class ProjectsTechnologiesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 0; $i < 10; $i++){
+
+          // ESTRAGGO UN PROGETTO RANDOM
+          $project = Project::inRandomOrder()->first();
+
+          // ESTRAGGO L-ID DI TECHNOLOGY RANDOM
+          $technology_id = Technology::inRandomOrder()->first()->id;
+
+          $project->technologies()->attach($technology_id);
+        }
+    }
+}
